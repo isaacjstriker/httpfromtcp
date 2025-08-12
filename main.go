8e5229ae2fa0a18882955f4b1abf2bb65e3fcc7f
isaac <isaacstriker@proton.me>
2025-08-12 12:10:43 -0400

    - Now have reusable function for getLinesChannel

--- main.go
+++ main.go
@@ -7,43 +7,57 @@ import (
 	"strings"
 )
 
-func main() {
-	// Open messages.txt for reading
+func getLinesChannel (f io.ReadCloser) <-chan string {
+	lines := make(chan string)
+
 	file, err := os.Open("messages.txt")
 	if err != nil {
-		err = fmt.Errorf("could not open file: %w", err)
-		fmt.Println(err.Error())
+		fmt.Println(err)
+		return nil
+	}
+
+	buffer := make([]byte, 8)
+	currentLine := ""
+
+	go func(){
+		defer file.Close()
+		defer close(lines)
+		for {
+			n, err := file.Read(buffer)
+			if err == io.EOF {
+				// End of file reached
+				break
+			}
+			if err != nil {
+				fmt.Printf("error reading file: %v\n", err)
+				return
+			}
+
+			parts := strings.Split(string(buffer[:n]), "\n")
+			// For each part, print a line to the console (except the last one)
+			for i := 0; i < len(parts) - 1; i++ {
+					lines <- currentLine + parts[i]
+					currentLine = ""
+			}
+			currentLine += parts[len(parts)-1]
+		}
+		if currentLine != "" {
+			lines <- currentLine
+		}
+	}()
+	return lines
+}
+
+func main() {
+	file, err := os.Open("messages.txt")
+	if err != nil {
+		fmt.Println(err)
 		return
 	}
 	defer file.Close()
 
-	buffer := make([]byte, 8)
-
-	currentLine := ""
-
-	// Loop to read every 8 bytes
-	for {
-		n, err := file.Read(buffer)
-		if err == io.EOF {
-			// End of file reached
-			break
-		}
-		if err != nil {
-			fmt.Printf("error reading file: %v\n", err)
-			return
-		}
-
-		parts := strings.Split(string(buffer[:n]), "\n")
-
-		// For each part, print a line to the console (except the last one)
-		for i := 0; i < len(parts) - 1; i++ {
-				fmt.Printf("read: %s\n", currentLine + parts[i])
-				currentLine = ""
-		}
-		currentLine += parts[len(parts)-1]
-	}
-
-	if currentLine != "" {
-		fmt.Printf("read: %s\n", currentLine)
+	lines := getLinesChannel(file)
+	for line := range lines {
+		fmt.Printf("read: %s\n", line)
 	}
 }
